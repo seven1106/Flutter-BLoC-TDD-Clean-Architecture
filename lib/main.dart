14b59183b2cfb61932c21956abf1bcca8e27206c
seven1106 <that9011@gmail.com>
2024-03-31 22:40:49 +0700

    setup supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_supabase_forums/core/theme/theme.dart';
+import 'package:flutter_tdd_clean_architecture/core/secrets/app_secrets.dart';
+import 'package:flutter_tdd_clean_architecture/core/theme/theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'features/auth/presentation/pages/sign_up_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supaBase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+    debug: true,
+  );
   runApp(const MyApp());
 }
 
